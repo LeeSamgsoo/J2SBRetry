8aabdba747c17290a1e4eb3b2e73c9fc9ab3f9f4
Leesamgsoo <sslee3680035@gmail.com>
2024-08-29 13:31:18 +0900

    점스부 2-10

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -1,25 +1,18 @@
 package com.mysite.sbb.question;
 
-import com.mysite.sbb.answer.Answer;
-import com.mysite.sbb.answer.AnswerRepository;
-import com.mysite.sbb.answer.AnswerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
-import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
 public class QuestionController {
 
     private final QuestionService questionService;
-
-    private final AnswerService answerService;
 
     @GetMapping("/question/list")
     public String list(Model model) {
@@ -28,7 +21,7 @@ public class QuestionController {
         return "question_list";
     }
 
-    @GetMapping("question/detail/${id}")
+    @GetMapping("/question/detail/{id}")
     public String detail(Model model, @PathVariable("id") Integer id) {
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question", question);
