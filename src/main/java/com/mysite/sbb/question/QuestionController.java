6bd6bc6a1ff924409568729ab6ccacd251953b03
Leesamgsoo <sslee3680035@gmail.com>
2024-08-29 13:03:34 +0900

    임시 커밋

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -2,10 +2,13 @@ package com.mysite.sbb.question;
 
 import com.mysite.sbb.answer.Answer;
 import com.mysite.sbb.answer.AnswerRepository;
+import com.mysite.sbb.answer.AnswerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,17 +17,21 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class QuestionController {
 
-    private final QuestionRepository questionRepository;
+    private final QuestionService questionService;
 
-    private final AnswerRepository answerRepository;
+    private final AnswerService answerService;
 
     @GetMapping("/question/list")
     public String list(Model model) {
-        Optional<Question> optionalQuestion = this.questionRepository.findById(2);
-        Question question = optionalQuestion.get();
-        List<Answer> answerList = this.answerRepository.findAllByQuestion(question);
-        model.addAttribute("answerList", answerList);
-        model.addAttribute("question", question);
+        List<Question> questionList = this.questionService.getList();
+        model.addAttribute("questionList", questionList);
         return "question_list";
+    }
+
+    @GetMapping("question/detail/${id}")
+    public String detail(Model model, @PathVariable("id") Integer id) {
+        Question question = this.questionService.getQuestion(id);
+        model.addAttribute("question", question);
+        return "question_detail";
     }
 }
