e432123637a7e2ff5a37df865019c0cc3325ebe9
Leesamgsoo <sslee3680035@gmail.com>
2024-08-29 09:21:50 +0900

    점스부 2-05 수정

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -26,11 +26,17 @@ class SbbApplicationTests {
     @Test
     @Transactional
     void contextLoads() {
-        Optional<Question> optionalQuestion = this.questionRepository.findById(2);
-        assertTrue(optionalQuestion.isPresent());
-        List<Answer> answerList = optionalQuestion.get().getAnswerList();
-        assertEquals(1, answerList.size());
-        assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
+        Question q1 = new Question();
+        q1.setSubject("sbb가 무엇인가요?");
+        q1.setContent("sbb에 대해서 알고 싶습니다.");
+        q1.setCreateDate(LocalDateTime.now());
+        this.questionRepository.save(q1);
+
+        Question q2 = new Question();
+        q2.setSubject("스프링부트 모델 질문입니다.");
+        q2.setContent("id는 자동으로 생성되나요?");
+        q2.setCreateDate(LocalDateTime.now());
+        this.questionRepository.save(q2);
     }
 
 }
